a0dfb14f03b2dcaef23eaae7f347d384b73831e7
Neisser <villa.neisser@gmail.com>
2024-01-27 13:47:52 -0500

    ✏️ refactor: separate models into different files

--- pkg/models/nodifications.model.go
+++ pkg/models/nodifications.model.go
@@ -1,10 +1,5 @@
 package models
 
-type User struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
 type Notification struct {
 	From    User   `json:"from"`
 	To      User   `json:"to"`
